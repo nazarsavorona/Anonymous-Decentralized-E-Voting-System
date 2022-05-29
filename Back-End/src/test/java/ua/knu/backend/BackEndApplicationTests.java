9318a683b8de399b63f5b649d8fc8951520f5187
Nazarii Savorona <54802030+nazarsavorona@users.noreply.github.com>
2022-05-29 16:57:09 +0300

    Add generated code

---
+++ Back-End/src/test/java/ua/knu/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.knu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
